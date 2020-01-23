2321ad46b5d478158de77009d199675f56c2670d
Terranova Enrico <terranovaenrico@MacBook-Pro-2.local>
2020-01-23 09:03:30 +0100

    call api restaurant finish

--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -1,9 +1,61 @@
 import express, {Request, Response, NextFunction, Router} from 'express';
 import bodyParser from 'body-parser';
+import { Schema } from 'mongoose';
+import { dbConnect } from '../dbConnection/connection';
 const router: Router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
+const Restaurant = require('../models/restaurant');
+const Order = require('../models/order');
 
 
+
+router.get('/', async (req: Request, res: Response) => {
+    if(req.query.name) {
+        let restaurant = await Restaurant.findOne({ name: req.query.name }).exec();
+        return res.json(restaurant);
+    }
+});
+
+router.post('/', async(req: Request, res: Response) => {
+    try{
+        let restaurant = await Restaurant.findOne({ name: req.body.name }).exec();
+        if(restaurant){
+            return res.status(403).send({message: 'Restaurant already esist'});
+        }else{
+            let newRestaurant = new Restaurant(req.body);
+            let result = await newRestaurant.save();
+            res.json({
+                restaurant: result
+            });
+        }
+    }catch(error) {
+        return res.status(500).send(error);
+    }
+});
+
+router.put('/:name', async (req: Request, res: Response) => {
+        await Restaurant.findOneAndUpdate({ name: req.params.name}, req.body, { new: true }).exec((err: Error, doc: any) => {
+            if(err) {
+                return res.status(404).send('Restaurant not found...');
+            }
+            return res.json({
+                message: 'Restaurant updated',
+                document: doc
+        });
+    });
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    await Restaurant.findOneAndDelete({id: req.params._id}).exec((err: Error, doc: any) => {
+        if(err) {
+            return res.status(404).send(err);
+        }
+        return res.json({
+            message: 'Restaurant deleted',
+            document: doc
+        });
+    });
+});
 
 export = router;
