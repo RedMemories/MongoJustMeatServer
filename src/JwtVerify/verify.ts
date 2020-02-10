ea9ebb9e7d0c12c746d8f37dcd9567c78405d2c0
domenicosf92 <signorefioredomenico@tiscali.it>
2020-02-10 10:47:49 +0100

    change authorization on headers

--- src/JwtVerify/verify.ts
+++ src/JwtVerify/verify.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-    jwt.verify(req.query.token, 'FLIZsTmhpB', (err: Error) => {
+    jwt.verify(req.headers.authorization as string, 'FLIZsTmhpB', (err: Error) => {
         err ? res.status(401).send({ message: 'Unauthorized request' }) : next();
     }); 
 }
