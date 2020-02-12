addf76cc59013e8391b2b9bfee2b5ed345b253ee
enricoterranova000@gmail.com <enricoterranova000@gmail.com>
2020-02-12 09:26:41 +0100

    swagger 1.0.0

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import Orders from './routes/orders';
 import cors from 'cors';
 import { dbConnect } from './dbConnection/connection';
 import mongoose from 'mongoose';
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
+
+
 mongoose.set('debug', false);
 mongoose.set('useFindAndModify', false);
 
@@ -21,6 +25,9 @@ app.use(cors());
 app.use('/users', Users);
 app.use('/restaurants', Restaurants);
 app.use('/orders', Orders);
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));     
+
 
 app.listen(PORT, "Localhost", (err) => {
     if(err) {
